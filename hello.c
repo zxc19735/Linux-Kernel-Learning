2fd0d36730c0e15ff6ed4ed57f1efc2bc0532dd8
zxc19735 <hlinakbenson@gmail.com>
2026-01-29 18:21:44 +0800

    Experiment 1: Hello World Kernel Module

---
+++ hello.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+// 當你執行 insmod (載入模組) 時會跑這裡
+static int __init hello_init(void) {
+	printk(KERN_INFO "Hello! 這是我在 Kernel 的第一步\n");
+	return 0;
+}
+
+// 當你執行 rmmod (移除模組) 時會跑這裡
+static void __exit hello_exit(void) {
+	printk(KERN_INFO "Goodbye! 結束 Kernel 實驗\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Your Name");
+MODULE_DESCRIPTION("A simple Hello World Kernel Module");
